15f2d5cd5d6a5f0fa2133c711c118657138605d1
hossein101 <hossein_lotg@yahoo.com>
2016-07-12 18:43:49 +0430

    Create autoleave.lua

---
+++ plugins/autoleave.lua
@@ -0,0 +1,22 @@
+do
+-- Will leave the group if be added
+local function run(msg, matches)
+local bot_id = our_id -- your bot id
+   -- like local bot_id = 1234567
+    if matches[1] == 'leave' and is_admin(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+      send_large_msg("chat#id"..msg.to.id, 'این گروه برای @x_R_E_D_B_O_T_x نیست برای خرید صحیح به پیوی ربات بیایید با 198 تا پلاگین😍', ok_cb, false)
+      chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+      block_user("user#id"..msg.from.id,ok_cb,false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^[!/](leave)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
