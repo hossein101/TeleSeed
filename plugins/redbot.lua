ee322ce22afc1289a650d672e3d75ea38aa8cedd
hossein101 <hossein_lotg@yahoo.com>
2016-11-14 22:46:51 +0330

    Create redbot.lua

---
+++ plugins/redbot.lua
@@ -0,0 +1,30 @@
+do
+function run(msg, matches)
+return [[
+<b>RedBot V.1
+An advanced administration bot based on TG-CLI written in Lua Open source!</b>
+
+
+<b>Support Link:</b>
+https://telegram.me/joinchat/Cz-RM0BvyXEU_YAtBP1q6g
+
+<b>Admins:</b>
+@VAHSHI_TYPER <b>[Developer, Founder, Manager]</b>
+
+@O_ojust_sheydao_O <b>[Developer, Manager]</b>
+
+<i>Our channel:</i>
+
+@Red_Ch
+]]
+end
+return {
+description = "!buy",
+usage = " !buy",
+patterns = {
+"^Redbot$",
+"^redbot$"
+},
+run = run
+}
+end
